0e43f1f03a727fb58dfa5115661dec2f8db1c66d
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-04-08 14:33:03 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ea60f4a35384341f3bab41f9b79583b5.js"
+  "./precache-manifest.e7f2efb8d8b7c20251194e12d7ed4a49.js"
 );
 
 self.addEventListener('message', (event) => {
