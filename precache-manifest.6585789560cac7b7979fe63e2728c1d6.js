4530db81bdf69260d2ad90be20e30eb3d78feb2a
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-04-10 19:40:55 +0300

    Updates

---
+++ precache-manifest.6585789560cac7b7979fe63e2728c1d6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87f98169df477155f00bee02108cd3b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "274f91ac9769dc374333",
+    "url": "./static/css/main.23b437aa.chunk.css"
+  },
+  {
+    "revision": "ce1f0fee449da8ddd33e",
+    "url": "./static/js/2.a1a8d5c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a1a8d5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274f91ac9769dc374333",
+    "url": "./static/js/main.b12bde96.chunk.js"
+  },
+  {
+    "revision": "12c4e154b7e3522d400f",
+    "url": "./static/js/runtime-main.fb694b63.js"
+  },
+  {
+    "revision": "ceb9001319ffc642ef345a16470aac35",
+    "url": "./static/media/Infinity-1.5s-200px.ceb90013.svg"
+  },
+  {
+    "revision": "a860aa3c8fe78a96b03706400cc4790d",
+    "url": "./static/media/basket-icon.a860aa3c.svg"
+  },
+  {
+    "revision": "35916e8d0648303e15828293400bb045",
+    "url": "./static/media/favorite-icon.35916e8d.svg"
+  },
+  {
+    "revision": "35c22a7195ca469c4663cb8649ca52b2",
+    "url": "./static/media/search-icon.35c22a71.svg"
+  }
+]);
